fedcc6b525866d13c5751e46fefc5fec8f13a39a
Ana Campos <anajbc@al.insper.edu.br>
2024-03-12 16:37:25 -0300

    sons funcionando

--- Memória e Sequência de Números/jogo.py
+++ Memória e Sequência de Números/jogo.py
@@ -25,8 +25,8 @@ def inicializa(): ##-- não tem argumento porque apenas cria coisas sem depender
     #pygame.mixer.music.load('C:\Users\anaju\projeto.pygame\projeto-pygame-azul\codigo\sucess-1-6287.mp3')
     #pygame.mixer.music.play('Sucess-1-6287.mp3')
 
-    # assets['acerto_som'] = pygame.mixer.Sound('success-1-6297.mp3')
-    # assets['erro_som'] = pygame.mixer.Sound("negative_beeps-6008.mp3")
+    assets['acerto_som'] = pygame.mixer.Sound('success-1-6297.mp3')
+    assets['erro_som'] = pygame.mixer.Sound("negative_beeps-6008.mp3")
 
 
     window = pygame.display.set_mode((700, 500))
@@ -67,12 +67,12 @@ def recebe_eventos():
         if str(assets['num gerado']) == assets['num_digitado']: ## -- comparar sempre dous valores do mesmo tipo
             assets['contagem_acertos'] += 1
             assets['qtd_dígitos_seq_num'] += 1
-            # assets['acerto_som'].play()
+            assets['acerto_som'].play()
 
             
         else:
             assets['contagem_vidas'] -= 1
-            # assets['erro_som'].play()
+            assets['erro_som'].play()
         assets['fase'] = 'Memorizar'
         assets['tempo_passado'] = pygame.time.get_ticks()
         assets['num_digitado'] = '' ##-- tem de ficar depois do if de acerto/erro, pois se ficar antes os comandos debaixp serao ignorados--##
